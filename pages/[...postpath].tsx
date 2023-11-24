77aee70bce404aebe668176e53edea2ef7182c95
yusufakdeniz <yusuf_akdeniz@yahoo.com>
2023-11-24 11:49:49 +0300

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -13,8 +13,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 	const fbclid = ctx.query.fbclid;
 
 	// redirect if facebook is the referer or request contains fbclid
-	if (referringURL?.includes('facebook.com') || fbclid) {
-		return {
+	return {
 			redirect: {
 				permanent: false,
 				destination: `${
@@ -22,7 +21,6 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 				}`,
 			},
 		};
-	}
 	const query = gql`
 		{
 			post(id: "/${path}/", idType: URI) {
